74c139b6933827031bc1aec45416520e27613d2e
BigBeoU <chaupham.dev@gmail.com>
2023-10-02 01:25:32 +0200

    done banner with slide picture

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,3 @@
-// const collapsibles = document.querySelectorAll(".collapsible");
-// const burger = document.querySelectorAll(".burger__menu");
-
-// collapsibles.forEach((item) => {
-//    console.log(item);
-//    this.addEventListener("click", function () {
-//       item.classList.toggle("collapsible--expanded");
-//    });
-// });
-
 // Get all elements with the class "burger__menu"
 const burgerMenus = document.querySelectorAll(".burger__menu");
 
@@ -21,3 +11,31 @@ burgerMenus.forEach((item) => {
       }
    });
 });
+let currentSlide = 0;
+const images = [
+   "./assets/images/posts/Post thumbnail-1.png",
+   "./assets/images/posts/Post thumbnail-2.png",
+   "./assets/images/posts/Post thumbnail-3.png",
+   "./assets/images/posts/Post thumbnail-4.png",
+];
+
+//Slice picture
+
+function slideImage(direction) {
+   const sliderImage = document.getElementById("sliderImage");
+
+   sliderImage.style.opacity = 0;
+
+   sliderImage.addEventListener("transitionend", function handler() {
+      // Depending on direction, increment or decrement currentSlide
+      if (direction === "forward") {
+         currentSlide = (currentSlide + 1) % images.length;
+      } else if (direction === "backward") {
+         currentSlide = (currentSlide - 1 + images.length) % images.length;
+      }
+
+      sliderImage.src = images[currentSlide];
+      sliderImage.style.opacity = 1;
+      sliderImage.removeEventListener("transitionend", handler);
+   });
+}
