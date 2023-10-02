dec1103f4a4ed99877df3a771616b98725b7c8a2
BigBeoU <chaupham.dev@gmail.com>
2023-10-02 15:53:58 +0200

    done skeketon

--- src/js/main.js
+++ src/js/main.js
@@ -39,3 +39,133 @@ function slideImage(direction) {
       sliderImage.removeEventListener("transitionend", handler);
    });
 }
+
+const app = document.getElementById("articles--container");
+
+const sectionsData = [
+   {
+      imgSrc: "./assets/images/posts/Post thumbnail-1.png",
+      title: "Section 1",
+      content: "Published 12/07/2021",
+   },
+   {
+      imgSrc: "./assets/images/posts/Post thumbnail-2.png",
+      title: "Section 2",
+      content: "Published 12/07/2021.",
+   },
+   {
+      imgSrc: "./assets/images/posts/Post thumbnail-3.png",
+      title: "Section 3",
+      content: "Published 12/07/2021.",
+   },
+   {
+      imgSrc: "./assets/images/posts/Post thumbnail-4.png",
+      title: "Section 4",
+      content: "Published 12/07/2021.",
+   },
+];
+
+function createSections() {
+   sectionsData.forEach((sectionData, index) => {
+      const section = document.createElement("section");
+      section.id = `section${index + 1}`;
+      section.innerHTML = `
+   <img class="article__items" src="${sectionData.imgSrc}" alt="${sectionData.title}" loading="lazy">
+   <h1 class="article__items">${sectionData.title}</h1>
+    <p class="article__items">${sectionData.content}</p>
+    <p class="article__items text show-more-height">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+      nonumy eirmod
+      tempor
+      invidunt ut labore et
+      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+      clita kasd gubergren, no sea takimata sanctus est...p Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Minima voluptate quaerat blanditiis consequuntur ad, accusamus quasi maxime voluptatem, perferendis neque
+      voluptas esse dolor rem quas porro veniam doloribus autem nostrum. Delectus, enim nisi. Quas, perspiciatis,
+      temporibus voluptatem debitis porro quos quaerat corrupti vel tempore sapiente deleniti. Ad libero ipsum atque
+      vel nostrum tempora, molestiae non ex, minima nam quia fugit, doloribus ea molestias in ab. Ipsum dolorem
+      necessitatibus soluta aspernatur nam consequatur velit amet. Repellendus nobis recusandae voluptas! Doloribus
+      soluta dolore sit, odit libero vitae voluptates eos adipisci eaque reprehenderit corrupti illo, eveniet maxime
+      aut nobis delectus quibusdam nisi magni ipsum minus architecto voluptate possimus iure magnam! Laborum sequi
+      officiis quis cum nobis sed magnam dolor optio odio excepturi sunt qui expedita earum nulla, eius quod
+      architecto hic laudantium commodi perferendis labore fugiat dolore consequuntur? Necessitatibus perferendis
+      quod cupiditate doloremque? Temporibus ipsa ea numquam optio, molestiae accusantium delectus. Voluptatum
+      obcaecati quasi autem beatae sunt excepturi perferendis repellat nesciunt itaque ipsum, quaerat quisquam
+      voluptate ipsam qui modi error deserunt eius suscipit reiciendis! Dolorum rem nisi recusandae perferendis natus
+      sequi animi totam doloribus deleniti est accusantium possimus iusto quo soluta expedita vel non, veritatis quas
+      illum libero esse tenetur labore magni! Magnam.</p>
+
+    <button class="btn article__items show-more";">Read more</button>
+  </section>           
+        `;
+      app.appendChild(section);
+   });
+}
+
+createSections();
+
+document.querySelector(".show-more").addEventListener("click", function () {
+   var textElement = document.querySelector(".text");
+   var showMoreButton = document.querySelector(".show-more");
+   var btn = document.querySelector(".btn");
+   // if (textElement.classList.contains("show-more-height")) {
+   //    showMoreButton.textContent = "(Show More)";
+   // } else {
+   //    showMoreButton.textContent = "(Show Less)";
+   // }
+   textElement.classList.toggle("show-more-height");
+   // btn.style.display = "none";
+});
+
+// Array of sponsor logo URLs
+const sponsorLogos = [
+   "./assets//images//sponsors/sponsor-12.png",
+   "./assets//images//sponsors/sponsor-13.png",
+   "./assets//images//sponsors/sponsor-14.png",
+   "./assets//images//sponsors/sponsor-15.png",
+   "./assets//images//sponsors/sponsor-16.png",
+   "./assets//images//sponsors/sponsor-17.png",
+   "./assets//images//sponsors/sponsor-18.png",
+   "./assets//images//sponsors/sponsor-19.png",
+   "./assets//images//sponsors/sponsor-20.png",
+   "./assets//images//sponsors/sponsor-41.png",
+   "./assets//images//sponsors/sponsor-40.png",
+   "./assets//images//sponsors/sponsor-1.png",
+   "./assets//images//sponsors/sponsor-2.png",
+   "./assets//images//sponsors/sponsor-3.png",
+   "./assets//images//sponsors/sponsor-4.png",
+   "./assets//images//sponsors/sponsor-5.png",
+   "./assets//images//sponsors/sponsor-6.png",
+   "./assets//images//sponsors/sponsor-7.png",
+   "./assets//images//sponsors/sponsor-8.png",
+   "./assets//images//sponsors/sponsor-9.png",
+   "./assets//images//sponsors/sponsor-10.png",
+   "./assets//images//sponsors/sponsor-11.png",
+];
+
+const instagramFeedArr = [
+   "./assets/images/instagram-feed/instagram-image-1.png",
+   "./assets/images/instagram-feed/instagram-image-2.png",
+   "./assets/images/instagram-feed/instagram-image-3.png",
+   "./assets/images/instagram-feed/instagram-image-4.png",
+   "./assets/images/instagram-feed/instagram-image-5.png",
+   "./assets/images/instagram-feed/instagram-image-6.png",
+   "./assets/images/instagram-feed/instagram-image-7.png",
+   "./assets/images/instagram-feed/instagram-image-8.png",
+   "./assets/images/instagram-feed/instagram-image-9.png",
+];
+
+const renderImage = (id, arr) => {
+   const sponsorContainer = document.getElementById(id);
+
+   // Render the sponsor logos using innerHTML and template literals
+   sponsorContainer.innerHTML = arr
+      .map(
+         (logoUrl) => `
+      <img src="${logoUrl}" alt="Sponsor Logo" class="sponsor-logo">
+   `,
+      )
+      .join("");
+};
+
+renderImage("sponsor-container", sponsorLogos);
+renderImage("instagram-feed", instagramFeedArr);
